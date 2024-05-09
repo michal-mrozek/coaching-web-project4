e848ed1767abba02472cb5bd7441a76ef4f21171
Michal Mrozek <m.mrozek@hotmail.com>
2024-05-10 00:29:10 +0100

    Portal - chat ready

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from profiles.models import UserProfile
 
@@ -5,11 +7,22 @@ class MessageChat(models.Model):
     """
     A user profile model for maintaining subscription information.
     """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_profile = models.ForeignKey('profiles.UserProfile', null=True, blank=True, on_delete=models.SET_NULL)
     datetime = models.DateTimeField(auto_now_add=True)
-    subject = models.CharField(max_length=255)
+    subject = models.ForeignKey('Subject', null=True, blank=True, on_delete=models.SET_NULL)
     message = models.TextField()
 
     def __str__(self):
         return self.user_profile.user.username
+
+
+class Subject(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    message_subject = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.message_subject
+
+
 
--- portal/views.py
+++ portal/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
-from .models import MessageChat
+from django.shortcuts import render, redirect, reverse,get_object_or_404
+from .models import MessageChat, Subject
 from profiles.models import UserProfile
 
 
-# Create your views here.
 def work_with_me(request):
     """ A view to return the reason page """
 
@@ -13,27 +12,58 @@ def work_with_me(request):
 def portal(request):
     """ A view to return the reason page """
     messages = MessageChat.objects.all()
-    mydata = MessageChat.objects.values_list('subject')
+    topics = Subject.objects.all()
+    topic_list = []
+    for topic in topics:
+        record = ["", ""]
+        for message in messages:
+            if message.subject == topic:
+                if record[1] == "" or message.datetime > record[1]:
+                    record = [topic, message.datetime]
+        topic_list.append(record)
 
     if request.method == 'POST':
+
         profile = UserProfile.objects.get(user=request.user)
-        subject = request.POST['subject']
+        topic = Subject.objects.create(message_subject=request.POST['subject'])
+        subject = topic
         message = request.POST['message']
         obj = MessageChat(user_profile=profile, subject=subject, message=message)
         obj.save()
+        return redirect(reverse('portal'))
 
     context = {
-        'messages': messages,
-        'mydata': mydata,
+        'messages': topic_list,
     }
 
     return render(request, 'portal/portal.html', context)
 
-# def portal_replay(request, subject):
-#     if request.method == 'POST':
-#         profile = UserProfile.objects.get(user=request.user)
-#         message = request.POST['message']
-#         obj = MessageChat.objects.get(user_profile=profile, subject=subject , message=message)
-#         obj.save()
 
+def portal_replay(request, subject_id):
+    messages = MessageChat.objects.all()
+    topics = Subject.objects.all()
+    topic_list = []
+    for topic in topics:
+        record = ["", ""]
+        for message in messages:
+            if message.subject == topic:
+                if record[1] == "" or message.datetime > record[1]:
+                    record = [topic, message.datetime]
+        topic_list.append(record)
+    conversation = messages.filter(subject=get_object_or_404(Subject, pk=subject_id))
 
+    if request.method == 'POST':
+        profile = UserProfile.objects.get(user=request.user)
+        message = request.POST['message']
+        subject = Subject.objects.get(pk=subject_id)
+        MessageChat.objects.create(user_profile=profile, subject=subject, message=message)
+        return redirect(reverse('portal'))
+
+    context = {
+        'messages': topic_list,
+        'conversation': conversation,
+        'id': subject_id,
+
+    }
+
+    return render(request, 'portal/portal.html', context)
